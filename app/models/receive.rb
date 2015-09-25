3e3015252aae6fe0e0e73067065368b90749468b
Eduardo <eduardo.s.tanaka@gmail.com>
2015-09-24 22:21:48 -0300

    removido classes não usadas

--- app/models/receive.rb
+++
@@ -1,9 +0,0 @@
-class Receive < Goliath::API
-  @@redis = Redis.new(Options::redis)
-  def response(env)
-    channel = env["REQUEST_PATH"][1..-1]
-    message = Rack::Utils.escape_html(params["message"])
-    @@redis.publish(channel, {sender: params["sender"], message: message}.to_json)
-    [ 200, { }, [ ] ]
-  end
-end
